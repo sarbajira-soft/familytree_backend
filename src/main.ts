0d5d360185892313a0825331b23f257bca9ccb17
ramansarbajira <raman.s@sarbajira.com>
2025-08-08 12:42:06 +0530

    updated code

--- src/main.ts
+++ src/main.ts
@@ -34,22 +34,6 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
   });
-<<<<<<< HEAD
-
-  // const sequelize = app.get(Sequelize);
-  // await sequelize.sync({ force: false, alter: true });
-  // console.log('Database synchronization successful.');
-
-  const swaggerPath = '/api';
-  setupSwagger(app, swaggerPath);
-
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-
-  const baseUrl = await app.getUrl();
-  console.log(`Application is running on: ${baseUrl}`);
-  console.log(`Swagger UI is available on: ${baseUrl}${swaggerPath}`);
-=======
 
   const sequelize = app.get(Sequelize);
   await sequelize.sync({ force: false, alter: true });
@@ -69,7 +53,6 @@ async function bootstrap() {
   await app.listen(process.env.PORT || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
   console.log(`Swagger UI is available on: ${await app.getUrl()}${isLambda ? '/' : '/api'}`);
->>>>>>> fa20b5721992d820e302d3d2fc2499aeea5908fb
 }
 
 bootstrap();
